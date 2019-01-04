1d5afd6b0763f6f3ed34c3a8c25f3e2a3d67bdef
AndreCamargodeBarros <acamargobarroscl@gmail.com>
2019-01-04 14:51:03 -0200

    Update calculadora.js
    ajuste de teclas

--- calculadora.js
+++ calculadora.js
@@ -35,34 +35,34 @@ function calculadora(){//Insere o Codigo HTML
                                 '<tr>'+
                                     '<td><input class="buttonCalc" type="button" value="C" onclick="limpa()"></td>'+
                                     '<td><input class="buttonCalc" type="button" value="<" onclick="back()"></td>'+
-                                    '<td><input class="buttonCalc" type="button" value="&divide;" onclick="insert('+divi+')"></td>'+
-                                    '<td><input class="buttonCalc" type="button" value="x" onclick="insert('+mult+')"></td>'+
                                     '<td><input class="buttonCalc" type="button" value="(" onclick="insert('+aspa+')"></td>'+
                                     '<td><input class="buttonCalc" type="button" value=")" onclick="insert('+aspf+')"></td>'+
                                 '</tr>'+
                                 '<tr>'+
-                                    '<td><input class="buttonCalc" type="button" value="9" onclick="insert(9)"></td>'+
-                                    '<td><input class="buttonCalc" type="button" value="8" onclick="insert(8)"></td>'+
                                     '<td><input class="buttonCalc" type="button" value="7" onclick="insert(7)"></td>'+
-                                    '<td><input class="buttonCalc" type="button" value="-" onclick="insert('+sub+')"></td>'+
+                                    '<td><input class="buttonCalc" type="button" value="8" onclick="insert(8)"></td>'+
+                                    '<td><input class="buttonCalc" type="button" value="9" onclick="insert(9)"></td>'+
+                                    '<td><input class="buttonCalc" type="button" value="&divide;" onclick="insert('+divi+')"></td>'+
                                     '<td><input class="buttonCalc" type="button" value="&prod;" onclick="pi()"></td>'+
                                     '<td><input class="buttonCalc" type="button" value="&radic;" onclick="insert('+raiz+')"></td>'+
                                 '</tr>'+
                                 '<tr>'+
-                                    '<td><input class="buttonCalc" type="button" value="6" onclick="insert(6)"></td>'+
-                                    '<td><input class="buttonCalc" type="button" value="5" onclick="insert(5)"></td>'+
                                     '<td><input class="buttonCalc" type="button" value="4" onclick="insert(4)"></td>'+
-                                    '<td><input class="buttonCalc" type="button" value="+" onclick="insert('+soma+')"></td>'+
-                                '</tr>'+
+                                    '<td><input class="buttonCalc" type="button" value="5" onclick="insert(5)"></td>'+
+                                    '<td><input class="buttonCalc" type="button" value="6" onclick="insert(6)"></td>'+
+                                    '<td><input class="buttonCalc" type="button" value="x" onclick="insert('+mult+')"></td>'+
+                                    '</tr>'+
                                 '<tr>'+
-                                    '<td><input class="buttonCalc" type="button" value="3" onclick="insert(3)"></td>'+
-                                    '<td><input class="buttonCalc" type="button" value="2" onclick="insert(2)"></td>'+
                                     '<td><input class="buttonCalc" type="button" value="1" onclick="insert(1)"></td>'+
+                                    '<td><input class="buttonCalc" type="button" value="2" onclick="insert(2)"></td>'+
+                                    '<td><input class="buttonCalc" type="button" value="3" onclick="insert(3)"></td>'+
+                                    '<td><input class="buttonCalc" type="button" value="-" onclick="insert('+sub+')"></td>'+
                                     '<td rowspan="2"><input style="height:88" class="buttonCalc" type="button" value="=" onclick="equal()"></td>'+
                                 '</tr>'+
                                 '<tr>'+
                                     '<td colspan="2"><input style="width:88" class="buttonCalc" type="button" value="0" onclick="insert(0)"></td>'+
                                     '<td><input class="buttonCalc" type="button" value="." onclick="insert('+ponto+')"></td>'+
+                                    '<td><input class="buttonCalc" type="button" value="+" onclick="insert('+soma+')"></td>'+
                                 '</tr>'+
                             '</table>'+
                     '</form>';
